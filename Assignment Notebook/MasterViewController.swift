2e363dee054b891476e18f62ba064f1c681bfb4e
Alex Zhou <zhoua2021@gmail.com>
2019-07-08 15:29:49 -0500

    Finishing touches

--- Assignment Notebook/MasterViewController.swift
+++ Assignment Notebook/MasterViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
+    var assignments = [Assignment]()
 
 
     override func viewDidLoad() {
@@ -30,13 +30,37 @@ class MasterViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     @objc
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+        let alert = UIAlertController(title: "Add Assignment", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Title"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Course(Subject)"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Due Date"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Description"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let insertAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let titleTextField = alert.textFields![0] as UITextField
+            let courseTextField = alert.textFields![1] as UITextField
+            let dueDateTextField = alert.textFields![2] as UITextField
+            let descriptionTextField = alert.textFields![3] as UITextField
+            let assignment = Assignment(title: titleTextField.text!, course: courseTextField.text!, dueDate: dueDateTextField.text!, description: descriptionTextField.text!)
+            self.assignments.append(assignment)
+            self.tableView.reloadData()
+        }
+        alert.addAction(insertAction)
+        present(alert, animated: true, completion: nil)
     }
 
     // MARK: - Segues
@@ -44,7 +68,7 @@ class MasterViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = assignments[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
@@ -53,20 +77,19 @@ class MasterViewController: UITableViewController {
         }
     }
 
-    // MARK: - Table View
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return assignments.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
+        let object = assignments[indexPath.row]
         cell.textLabel!.text = object.description
         return cell
     }
@@ -78,13 +101,17 @@ class MasterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            assignments.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let objectToMove = assignments.remove(at: sourceIndexPath.row)
+        assignments.insert(objectToMove, at: destinationIndexPath.row)
+    }
 
 }
 
